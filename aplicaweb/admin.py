687db1f6d0c9c1e497e828cc63971d5bed6a516c
melfer <dacosta50@uan.edu.co>
2020-10-31 18:24:44 -0500

    actualizar aplicaweb

--- aplicaweb/admin.py
+++ aplicaweb/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import descuento
+
+class descuentoadmin(admin.modeladmin):
+    pass
+
 # Register your models here.
+admin.site.register(descuento,descuentoadmin)
--- aplicaweb/models.py
+++ aplicaweb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class descuento(models.model):
+    numdefactura = models.charfield(max lengeth=34)
+    nombre = models.charfield(max lengeth=40)
+    infdefactura = models.charfield(max lendeth=64)
+    usuario = models.charfield(max lengeth=45)
+    clave = models.integerfield(max lengeth=25)

de4ffe03beef4de51d7444567df9cdcd08ceb3d0
melfer <dacosta50@uan.edu.co>
2020-11-27 18:21:19 -0500

    tablas de hechos actualizadas en django

--- aplicaweb/admin.py
+++ aplicaweb/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import descuento
+from .models import descuento, pago, cliente, usuario, tarjeta_de_credito, servicio_factura
 
-class descuentoadmin(admin.modeladmin):
+class descuentoAdmin(admin.ModelAdmin):
     pass
 
 # Register your models here.
-admin.site.register(descuento,descuentoadmin)
+
+admin.site.register(descuento, descuentoAdmin)
+admin.site.register(pago)
+admin.site.register(cliente)
+admin.site.register(usuario)
+admin.site.register(tarjeta_de_credito)
+admin.site.register(servicio_factura)
--- aplicaweb/models.py
+++ aplicaweb/models.py
@@ -2,9 +2,77 @@ from django.db import models
 
 # Create your models here.
 
-class descuento(models.model):
-    numdefactura = models.charfield(max lengeth=34)
-    nombre = models.charfield(max lengeth=40)
-    infdefactura = models.charfield(max lendeth=64)
-    usuario = models.charfield(max lengeth=45)
-    clave = models.integerfield(max lengeth=25)
+class descuento(models.Model):
+    numdefactura = models.IntegerField (max_length=43)
+    nombre = models.CharField (max_length=40)
+    infdefactura = models.CharField (max_length=64)
+    usuario = models.CharField (max_length=45)
+    clave = models.CharField(max_length= 20, blank= False, null= False)
+
+
+class pago(models.Model):
+    fecha_de_pago = models.DateField (auto_now_add=True)
+    forma_de_pago = models.CharField (max_length=50, verbose_name='Forma de pago')
+    num_de_operacion = models.IntegerField (max_length=64)
+    tipo_de_cuenta = models.CharField (max_length=45)
+    monto = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+    
+
+
+class cliente(models.Model):
+    nombre = models.CharField (max_length=50)
+    direccion = models.CharField (max_length=40)
+    telefono = models.CharField (max_length=64)
+    email = models.EmailField()
+    GENDER_CHOICES = (
+
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+    )
+    sexo = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    ciudad = models.CharField (max_length=50)
+    estado = models.CharField (max_length=40)
+
+
+class usuario(models.Model):
+    nombre = models.CharField (max_length=50)
+    apellido = models.CharField (max_length=40)
+    username = models.CharField(max_length=100)
+    contraseña = models.CharField(max_length= 20, blank= False, null= False)
+    email = models.EmailField(max_length=100)
+
+
+class tarjeta_de_credito(models.Model):
+
+    GENDER_CHOICES = (
+
+        ('C', 'CREDITO'),
+        ('D', 'DEBITO'),
+    )
+
+    tipo_de_tarjeta = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    fecha_de_vencimiento = models.DateField(null=True)
+    plazo = models.IntegerField (max_length=64)
+    num_de_cuenta = models.IntegerField(max_length=8, verbose_name='Numero de cuenta')
+    
+    GENDER_CHOICES = (
+
+        ('S', 'SI'),
+        ('N', 'NO'),
+    )
+    
+    disponible = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+
+
+
+class servicio_factura(models.Model):
+    nom_de_factura = models.CharField (max_length=43)
+    numdefacrura = models.IntegerField (max_length=40)
+    fecha_de_corte = models.DateField(null=True)
+    fecha_de_pago = models.DateField(null=True)
+    descripcion_fac = models.CharField (max_length=54)
+    
+    
